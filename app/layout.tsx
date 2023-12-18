c21a9db8e60f299cad8c39e8157d8849e0aa050d
aphuus <aphuus@utu.fi>
2023-12-18 16:23:07 +0200

    search init

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body className={cn('bg-secondary', inter.className)}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
